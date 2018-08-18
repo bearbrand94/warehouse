e0eed2a6de27ca6a5cd122ba512832f7817eea10
bearbrand94 <paulusw.94@gmail.com>
2018-08-18 12:28:49 +0700

    first comm

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //yajra datatables
+        Yajra\Datatables\DatatablesServiceProvider::class,
+
     ],
 
     /*
